355ba68df783991e9d557e767803dbf2ec9e4c91
Akhilesh Sivaraman <66359910+akhileshsivaraman@users.noreply.github.com>
2023-04-15 17:22:10 +0100

    Add files via upload

---
+++ Mastering Shiny/Basic Reactivity/app.R
@@ -0,0 +1,85 @@
+#---- Reactive expressions ----
+
+#---- libraries ----
+library(shiny)
+library(ggplot2)
+
+#---- define functions ----
+# function to visualise two distributions
+freqpoly <- function(x1, x2, binwidth = 0.1, xlim = c(-3, 3)) {
+  df <- data.frame(
+    x = c(x1, x2),
+    g = c(rep("x1", length(x1)), rep("x2", length(x2)))
+  )
+  
+  ggplot(df, aes(x, colour = g)) +
+    geom_freqpoly(binwidth = binwidth, linewidth = 1) +
+    coord_cartesian(xlim = xlim)
+}
+
+# use a t-test to compare means and summarise the results
+t_test <- function(x1, x2) {
+  test <- t.test(x1, x2)
+  
+  # use sprintf() to format t.test() results compactly
+  sprintf(
+    "p value: %0.3f\n[%0.2f, %0.2f]",
+    test$p.value, test$conf.int[1], test$conf.int[2]
+  )
+}
+
+
+#---- the app ----
+ui <- fluidPage(
+  fluidRow(
+    column(4, 
+           "Distribution 1",
+           numericInput("n1", label = "n", value = 1000, min = 1),
+           numericInput("mean1", label = "µ", value = 0, step = 0.1),
+           numericInput("sd1", label = "σ", value = 0.5, min = 0.1, step = 0.1)
+    ),
+    column(4, 
+           "Distribution 2",
+           numericInput("n2", label = "n", value = 1000, min = 1),
+           numericInput("mean2", label = "µ", value = 0, step = 0.1),
+           numericInput("sd2", label = "σ", value = 0.5, min = 0.1, step = 0.1)
+    ),
+    column(4,
+           "Frequency polygon",
+           numericInput("binwidth", label = "Bin width", value = 0.1, step = 0.1),
+           sliderInput("range", label = "range", value = c(-3, 3), min = -5, max = 5)
+    )
+  ),
+  fluidRow(
+    column(9, plotOutput("hist")),
+    column(3, verbatimTextOutput("ttest"))
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  # in this app, x1 and x2, and consequently, hist and t-test, are updated whenever one of the inputs are updated
+  # similarly, when one of the inputs in freqpoly is updated, everything else in renderPlot is recalculated
+  # another problem with this app is that x1 and x2 are calculated twice: once for the plot and again for the t-test
+  # reactive expressions would solve these issues and make the app more efficient
+  # x1 <- reactive(rnorm(input$n1, input$mean1, input$sd1))
+  # x2 <- rnorm(input$n2, input$mean2, input$sd2)
+  
+  output$hist <- renderPlot({
+    x1 <- rnorm(input$n1, input$mean1, input$sd1)
+    x2 <- rnorm(input$n2, input$mean2, input$sd2)
+    
+    freqpoly(x1, x2, binwidth = input$binwidth, xlim = input$range)
+  }, res = 96)
+  
+  output$ttest <- renderText({
+    x1 <- rnorm(input$n1, input$mean1, input$sd1)
+    x2 <- rnorm(input$n2, input$mean2, input$sd2)
+    
+    t_test(x1, x2)
+  })
+}
+
+
+shinyApp(ui, server)
